d3b38bc57f49960b1c5009e5bed309c1bc0d9451
Eduardo Mury Assafin <101987520+Dudzz01@users.noreply.github.com>
2024-03-07 16:33:46 -0300

    update criando conexao busca front end + backend

---
+++ src/main/View/buscaCliente/buscaCliente.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function (ev)
+
+{
+    ev.preventDefault();
+
+    document.getElementById("busca-cliente-form").addEventListener("submit",async function (ev)
+    {
+        ev.preventDefault();
+
+        fetch("")
+
+    }
+    )
+
+}
+
+)
