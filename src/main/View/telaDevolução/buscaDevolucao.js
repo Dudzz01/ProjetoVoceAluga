4199e742d668c56a65177cee8560e28686ba9911
Eduardo Mury Assafin <101987520+Dudzz01@users.noreply.github.com>
2024-05-14 16:50:07 -0300

    insert buscardevolucao frontend

---
+++ src/main/View/telaDevolução/buscaDevolucao.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function (ev)
+{
+    ev.preventDefault();
+    document.getElementById("devolucaoBusca").addEventListener("submit",function (ev)
+    {
+        ev.preventDefault();
+
+    })
+})
