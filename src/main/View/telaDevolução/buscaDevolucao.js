a207433df1a3468a45b785c8164b65e99b41067b
Eduardo Mury Assafin <101987520+Dudzz01@users.noreply.github.com>
2024-05-15 14:48:27 -0300

    update tela busca devolucao front+backend

--- src/main/View/telaDevolução/buscaDevolucao.js
+++ src/main/View/telaDevolução/buscaDevolucao.js
@@ -5,5 +5,27 @@ document.addEventListener("DOMContentLoaded",function (ev)
     {
         ev.preventDefault();
 
+        var codDevolucao = document.getElementById("buscaDevolucao").value;
+
+        fetch("http://localhost:8080/devolucao/"+codDevolucao).then(response => response.json()).then(function (devolucaoObject)
+
+            {
+                document.getElementById("cod_devol").innerText = devolucaoObject.id
+                document.getElementById("cpf_cliente").innerText = devolucaoObject.cliente.cpf
+                document.getElementById("cpf_motorista").innerText = devolucaoObject.locacao.motorista.cpf
+                document.getElementById("codigo_veiculo").innerText = devolucaoObject.veiculo.id
+                document.getElementById("nome_veiculo").innerText = devolucaoObject.veiculo.nome
+                document.getElementById("data_pedido").innerText = devolucaoObject.dataPedido
+                document.getElementById("cod_locacao").innerText = devolucaoObject.locacao.id
+                document.getElementById("end_retirada").innerText = devolucaoObject.locacao.enderecoRetirada
+                document.getElementById("end_devolucao").innerText = devolucaoObject.locacao.enderecoDevolucao
+                document.getElementById("cod_transacao").innerText = devolucaoObject.transacao.id
+                document.getElementById("data_fim_esperado").innerText = devolucaoObject.dataFimEsperado
+                document.getElementById("data_devolucao").innerText = devolucaoObject.dataDevolucao
+
+
+            }
+
+        )
     })
 })
